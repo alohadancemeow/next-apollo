011dfdab73a2f005f61cdb8dc7e2f6eb453017ce
Alohadancemeow <nawongsak@gmail.com>
2022-01-10 16:50:16 +0700

    adding styles

--- pages/products/index.js
+++ pages/products/index.js
@@ -5,34 +5,41 @@ const fakeData = [
     {
         id: 1,
         desc: 'Product1',
-        price: 150.00
+        price: 150.00,
+        imageUrl: 'https://images.unsplash.com/photo-1536998003793-b13c28fae74b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80'
     },
     {
         id: 2,
         desc: 'Product2',
-        price: 150.00
+        price: 150.00,
+        imageUrl: 'https://images.unsplash.com/photo-1536513953700-ba24c78a5f51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
     },
     {
         id: 3,
         desc: 'Product3',
-        price: 150.00
+        price: 150.00,
+        imageUrl: 'https://images.unsplash.com/photo-1536998003793-b13c28fae74b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80'
     },
 ]
 
 
 const products = () => {
     return (
-        <div>
+        <div className='product-list'>
             {fakeData.map(item => (
-                <Link
-                    key={item.id}
-                    href="/products[productId]"
-                    as={`products/${item.id}`}
-                >
-                    <a>
-                        <div>{item.desc}</div>
-                    </a>
-                </Link>
+                <div key={item.id} className='product-item'>
+                    <Link
+                        href="/products[productId]"
+                        as={`products/${item.id}`}
+                    >
+                        <a>
+                            <img src={item.imageUrl} alt={item.desc} width="250px" />
+                        </a>
+                    </Link>
+                    <h3>{item.desc}</h3>
+                    <h4>{item.price + " THB"}</h4>
+                    <button>Add to Cart</button>
+                </div>
             ))}
         </div>
     )
